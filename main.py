d9429714e8b8c66d433aa9e8d6d0b16cb5b95982
mahdibh <mahdibahramih@gmail.com>
2022-02-25 21:34:26 +0330

    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+#app.config['UPLOAD_FOLDER']
+app = Flask(__name__)
+app.config['MAX_CONTENT_PATH'] = '10240'
+app.config['UPLOAD_FOLDER'] = '/codes'
+
+@app.route('/')
+def index():
+    return "hello"
+
+@app.route('/GetCode' , methods=['POST'])
+def get_code():
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    return 'file uploaded successfully'
+
+@app.route('/GetCode' , methods=['GET'])
+def get_code_html():
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
