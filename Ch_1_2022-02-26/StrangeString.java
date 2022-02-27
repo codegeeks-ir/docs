b0136f6a1d53677c1278588fd486fb80f71e5e79
Mehdi Ebrahimi <mitiebh@gmail.com>
2022-02-28 00:45:04 +0330

    Update StrangeString.java
    algorithm did not work in some test case.

--- Ch_1_2022-02-26/StrangeString.java
+++ Ch_1_2022-02-26/StrangeString.java
@@ -1,47 +1,48 @@
 import java.util.Scanner;
 public class StrangeString
 {
+    static void Answer(String[] s1, String[] s2)
+    {
+        String[] arr = new String[s1.length*2];
+        for(int i=0; i<s1.length*2; i++)
+        {
+            if(i % 2 == 0)
+                arr[i] = s1[i/2];
+            if(i % 2 == 1)
+                arr[i] = s2[i/2];
+        }
+        String first_arr = arr[0];
+        String second_arr = arr[1];
+        for(int i=2; i<arr.length; i+=2)
+        {
+            if(first_arr.equals(arr[i]))
+            {
+                if(!second_arr.equals(arr[i+1]))
+                {
+                    System.out.println("False");
+                    System.exit(0);
+                }
+                continue;
+            }
+            if(!first_arr.equals(arr[i]))
+            {
+                if(second_arr.equals(arr[i+1]))
+                {
+                    System.out.println("False");
+                    System.exit(0);
+                }
+                continue;
+            }
+        }
+        System.out.println("True");
+    }
+
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
-
-        String[] input1 = scan.nextLine().split(""); // ex: a b b a
-        String[] input2 = scan.nextLine().split(" ");// ex: dog fish fish dog
-
-        int[] answerArr = new int[input1.length];
-        answerArr[0] = 1; // 1 is true.
-
-        for(int i=1; i<input1.length; i++)
-        {
-            if(input1[i].equals(input1[i-1]))
-            {
-                if(input2[i].equals(input2[i-1]))
-                {
-                    answerArr[i] = 1; // true
-                    continue;
-                }
-                answerArr[i] = 0; // false
-            }
-            else if(!input1[i].equals(input1[i-1]))
-            {
-                if(!input2[i].equals(input2[i-1]))
-                {
-                    answerArr[i] = 1; // true
-                    continue;
-                }
-                answerArr[i] = 0; // false
-            }
-        }
-        // chack if at least one 0 in answerArr return false.
-        for(int i=0; i<answerArr.length; i++)
-        {
-            if(answerArr[i] == 0)
-            {
-                System.out.println(false);
-                System.exit(0);
-            }
-        }
-        System.out.println(true);
+        String[] input1 = scan.nextLine().split("");  // ex: a b b a
+        String[] input2 = scan.nextLine().split(" "); // ex: dog fish fish dog
+        Answer(input1,input2);
         scan.close();
     }
 }
