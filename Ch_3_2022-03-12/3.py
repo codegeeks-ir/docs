8f74a2ac2e899dee8a92d39808a3f033c18fec5e
mahdibh <mahdibahramih@gmail.com>
2022-03-12 12:06:48 +0330

    add CH3

---
+++ Ch_3_2022-03-12/3.py
@@ -0,0 +1,21 @@
+caps = 0
+a = ""
+st1 = ""
+
+number = input()
+number = int(number)
+while number > 0 :
+    a = input()
+    if a == "CAPS" :
+        if caps == 1 :
+            caps = 0
+        else: 
+            caps = 1
+        continue
+
+    if caps == 1 :
+        a = a.capitalize()
+    st1 = st1 + a
+    number = number - 1
+
+print(st1)
