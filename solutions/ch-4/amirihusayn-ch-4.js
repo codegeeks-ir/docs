1ae409f49d6c3f583d2f14e4b54b4250e9213ee7
amirihusayn <amirgoodarzi75@gmail.com>
2022-03-19 20:46:16 +0330

    Add amirihusayn-ch-4.js

---
+++ solutions/ch-4/amirihusayn-ch-4.js
@@ -0,0 +1,66 @@
+function generateDieHards(length) {
+    const max = findMax(length);
+    const min = findMin(max);
+    const firstNumber = (min % 2 == 0) ? min + 1 : min;
+    const lastNumber = (max % 2 == 0) ? max - 1 : max;
+    const step = (min >= 10) ? 2 : 1;
+    for(let number = firstNumber; number <= lastNumber; number += step) {
+        if(isCheckNeeded(number))
+            if(isDieHard(number, min))
+                console.log(number);
+    }
+    readline.close();
+}
+
+function findMax(length) {
+    let max = 1;
+    for (let index = 1; index <= length; index++)
+        max *= 10;
+    return max--;
+}
+
+function findMin(max) {
+    return Math.floor((max + 1) / 10);
+}
+
+function isCheckNeeded(number) {
+    const isCheckNeeded = number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0;
+    return (number < 10) ? true : ( (isCheckNeeded) ? false : true);
+}
+
+function isDieHard(number, min) {
+    let dieHardFlag = true;
+    for(let divider = min; divider >= 1; divider /= 10) {
+        divider = Math.floor(divider);
+        if(!isPrime(Math.floor(number / divider))) {
+            dieHardFlag = false;
+            break;
+        }
+    }
+    return dieHardFlag;
+}
+
+function isPrime(number) {
+    let primeFlag = true;
+    let counter = 0;
+    if(number == 1)
+        return false;
+    for(let index = 1; index <= number; index++) {
+        if(number % index == 0)
+            counter++;
+        if(counter > 2) {
+            primeFlag = false;
+            break;
+        }
+    }
+    return primeFlag;
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.on( 'line', length => {
+    generateDieHards(length);
+});
